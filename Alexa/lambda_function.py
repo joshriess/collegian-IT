34842bc6f4c43eeda395934cf7d156fe8f76a135
pkmnct <george@georgewwalker.com>
2017-11-04 13:56:04 -0500

    Implement search

--- Alexa/lambda_function.py
+++ Alexa/lambda_function.py
@@ -133,10 +133,23 @@ def rightIntent(intent, session):
     return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 
 def searchIntent(intent, session):
+
+    r = requests.get('http://hack-kstate-escape.herokuapp.com/player/room')
+    data = json.loads(r.text)
+
+    currentRoom = data["room"];
+
+
+    r = requests.get('http://hack-kstate-escape.herokuapp.com/rooms/' + currentRoom)
+    data = json.loads(r.text)
+
+    searchObject = data["search"]
+    searchText = searchObject["text"]
+
     card_title = intent['name']
     session_attributes = {}
     should_end_session = False
-    speech_output = card_title
+    speech_output = searchText
     reprompt_text = "What would you like to do?"
     return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 
@@ -148,59 +161,6 @@ def catchAll(intent, session):
     reprompt_text = "Please try again"
     return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 
-#
-# def create_favorite_color_attributes(favorite_color):
-#     return {"favoriteColor": favorite_color}
-#
-#
-# def set_color_in_session(intent, session):
-#     """ Sets the color in the session and prepares the speech to reply to the
-#     user.
-#     """
-#
-#     card_title = intent['name']
-#     session_attributes = {}
-#     should_end_session = False
-#
-#     if 'Color' in intent['slots']:
-#         favorite_color = intent['slots']['Color']['value']
-#         session_attributes = create_favorite_color_attributes(favorite_color)
-#         speech_output = "I now know your favorite color is " + \
-#                         favorite_color + \
-#                         ". You can ask me your favorite color by saying, " \
-#                         "what's my favorite color?"
-#         reprompt_text = "You can ask me your favorite color by saying, " \
-#                         "what's my favorite color?"
-#     else:
-#         speech_output = "I'm not sure what your favorite color is. " \
-#                         "Please try again."
-#         reprompt_text = "I'm not sure what your favorite color is. " \
-#                         "You can tell me your favorite color by saying, " \
-#                         "my favorite color is red."
-#     return build_response(session_attributes, build_speechlet_response(
-#         card_title, speech_output, reprompt_text, should_end_session))
-#
-#
-# def get_color_from_session(intent, session):
-#     session_attributes = {}
-#     reprompt_text = None
-#
-#     if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-#         favorite_color = session['attributes']['favoriteColor']
-#         speech_output = "Your favorite color is " + favorite_color + \
-#                         ". Goodbye."
-#         should_end_session = True
-#     else:
-#         speech_output = "I'm not sure what your favorite color is. " \
-#                         "You can say, my favorite color is red."
-#         should_end_session = False
-#
-#     # Setting reprompt_text to None signifies that we do not want to reprompt
-#     # the user. If the user does not respond or says something that is not
-#     # understood, the session will end.
-#     return build_response(session_attributes, build_speechlet_response(
-#         intent['name'], speech_output, reprompt_text, should_end_session))
-#
 
 # --------------- Events ------------------
 
