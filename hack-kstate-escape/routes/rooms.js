8d44b413a3327a25eab1c3388be26611dd090d8f
Zach Martin <zmironman@gmail.com>
2017-11-03 23:33:56 -0500

    Added better search and more rooms
    Search now has two parts and I added startCorner, atruim, and radinas

--- hack-kstate-escape/routes/rooms.js
+++ hack-kstate-escape/routes/rooms.js
@@ -4,23 +4,82 @@ var router = express.Router();
 var rooms = {
   "start":{
     "picture":"",
-    "search":"You search the room, but find nothing.",
+    "search":,{
+      "text": "You search the room, but find nothing.",
+      "found":{
+        false
+      }
+    },
     "back":false,
     "left":"startCorner",
     "right":"beocat",
     "exit":false,
     "forward":false
   },
+
+
   "beocat":{
     "picture":"",
-    "search":"You try to open all the doors, but they are locked.",
+    "search":,{
+      "text": "You try to open all the doors, but they are locked.",
+      "found":{
+        false
+      }
+    },
     "back":"start",
     "left":false,
     "right":"startCorner",
     "exit":false,
     "forward":false
   },
+
+
+  "startCorner":{
+    "picture":"",
+    "search":,{
+      "text": "You see a coffee shop ahead.",
+      "found":{
+        false
+      }
+    },
+    "back":"start",
+    "left":false,
+    "right":"beocat",
+    "exit":false,
+    "forward":"atruim"
+  },
+
+
+  "atrium":{
+    "picture":"",
+    "search":,{
+      "text": "The coffe shop is closer.",
+      "found":{
+        false
+      }
+    },
+    "back":"startCorner",
+    "left":false,//Change for late game
+    "right":false,
+    "exit":true,
+    "forward":"radinas"
+  },
+
   
+  "radinas":{
+    "picture":"",
+    "search":{
+      "text": "You search the room, but find nothing.",
+      "found":{
+        "key"
+      }
+    },
+    "back":false,
+    "left":"startCorner",
+    "right":"beocat",
+    "exit":false,
+    "forward":false
+  },
 }
 
 /* GET rooms listing. */
