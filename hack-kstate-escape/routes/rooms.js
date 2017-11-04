0bc19f84539f3ff1b184df633f7a34cf4b0b3f24
Zach Martin <zmironman@gmail.com>
2017-11-03 23:16:38 -0500

    added rooms
    start and beocat

--- hack-kstate-escape/routes/rooms.js
+++ hack-kstate-escape/routes/rooms.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var rooms = {
+  "start":{
+    "picture":"",
+    "search":"You search the room, but find nothing.",
+    "back":false,
+    "left":"startCorner",
+    "right":"beocat",
+    "exit":false,
+    "forward":false
+  },
+  "beocat":{
+    "picture":"",
+    "search":"You try to open all the doors, but they are locked.",
+    "back":"start",
+    "left":false,
+    "right":"startCorner",
+    "exit":false,
+    "forward":false
+  },
+  
+}
+
 /* GET rooms listing. */
 router.get('/', function(req, res, next) {
 
