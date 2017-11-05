f7ce309a94bacd4b62825a236d025b969d621f1a
pkmnct <george@georgewwalker.com>
2017-11-05 01:16:28 -0500

    Added intro

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,60 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var page = `
-  <html style="background-size: cover;">
+  <html style="background-size: cover; background: url(/images/startRoom.jpg) no-repeat center center fixed;">
     <head>
         <title>Hack K-State Escape</title>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+        <style>
+            div, h1, h2 {
+                font-family: 'Roboto', sans-serif;
+            }
+            h1, h2 {
+                color: white;
+            }
+            .options {
+                font-size: 32px;
+                color: white;
+                position: absolute;
+                text-align: center;
+            }
+            #left {
+                top: 50%;
+                left: 5;
+            }
+            #right {
+                top: 50%;
+                right: 5;
+            }
+            #back {
+                bottom: 5;
+                left: 50%;
+                margin-left: -38px;
+            }
+            #forward {
+                top: 5;
+                left: 50%;
+                margin-left: -41px;
+            }
+            #search {
+                right: 5;
+                bottom: 5;
+            }
+            .indicator-done {
+                color: green;
+            }
+            .indicator-notsearched {
+                color: yellow;
+            }
+        </style>
     </head>
     <body style='width: 100%; height: 100%; padding: 0; margin: 0;'>
-        <div class="options" id="left" style="display: none;">&#9664;<br>Left</div>
-        <div class="options" id="right" style="display: none;">&#9654;<br>Right</div>
-        <div class="options" id="back" style="display: none;">Back<br>&#9660;</div>
-        <div class="options" id="forward" style="display: none;">&#9650;<br>Forward</div>
+        <div id="intro" style="width: 85%;padding-top:8%;margin-left:auto;margin-right:auto;text-align:center;"><img src="/images/logo.png" style="max-width: 80%;"><br><h1>Welcome to Hack K-State Escape!</h1><h2>Say "Alexa, play Escape" to get started, then say any command available on the screen.</h2></div>
+        <div class="options" id="left" style="display: none;">&#9664;<br>Left<br><i id="left-searched" class="fa"></i></div>
+        <div class="options" id="right" style="display: none;">&#9654;<br>Right<br><i id="right-searched" class="fa"></i></div>
+        <div class="options" id="back" style="display: none;"><i id="back-searched" class="fa"></i><br>Back<br>&#9660;</div>
+        <div class="options" id="forward" style="display: none;">&#9650;<br>Forward<br><i id="forward-searched" class="fa"></i></div>
         <div class="options" id="search" style="display: none;"><i class="fa fa-search" aria-hidden="true"></i><br>Search/Interact</div>
         <div id="cutscene" style="width: 100%; height: 100%; margin: 0; padding: 0;"><img src="" style="width: 100%; height: 100%; margin: 0px; padding: 0px;"></div>
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
@@ -23,6 +66,11 @@ router.get('/', function(req, res, next) {
             function loadThings() {
                 $.getJSON("/player/room", function(data) {
                     if (currentRoom != data.room) {
+                        if (data.room == "startRoom") {
+                            $("#intro").show()
+                        } else {
+                            $("#intro").hide()
+                        }
                         $("html").css("background","url(/images/" + data.room + ".jpg) no-repeat center center fixed");
                         var audio = new Audio("/audio/" + data.room + ".wav");
                         audio.play();
@@ -74,39 +122,6 @@ router.get('/', function(req, res, next) {
             }
             loadThings();
         </script>
-        <style>
-            div {
-                font-family: 'Roboto', sans-serif;
-            }
-            .options {
-                font-size: 32px;
-                color: white;
-                position: absolute;
-                text-align: center;
-            }
-            #left {
-                top: 50%;
-                left: 5;
-            }
-            #right {
-                top: 50%;
-                right: 5;
-            }
-            #back {
-                bottom: 5;
-                left: 50%;
-                margin-left: -38px;
-            }
-            #forward {
-                top: 5;
-                left: 50%;
-                margin-left: -41px;
-            }
-            #search {
-                right: 5;
-                bottom: 5;
-            }
-        </style>
     </body>
   </html>`;
   res.send(page);
