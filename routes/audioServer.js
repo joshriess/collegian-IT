df2c5d7f5b9d5a2317a9d2a451d52950d30c04a2
pkmnct <george@georgewwalker.com>
2017-11-05 00:44:25 -0500

    Enable 2 day caching for audio and images to prevent reloading

--- routes/audioServer.js
+++ routes/audioServer.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var dir = express.static('audio');
+var dir = express.static('audio', {
+  maxage: '48h'
+});
 router.use(dir);
 
 
--- routes/imageServer.js
+++ routes/imageServer.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var dir = express.static('images');
+var dir = express.static('images', {
+  maxage: '48h'
+});
 router.use(dir);
 
 
