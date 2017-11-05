4fb1512ebb452385e1ca0ed254a9e5e375e61b18
Zach Martin <zmironman@gmail.com>
2017-11-04 22:10:32 -0500

    fixed the easter egg

--- routes/rooms.js
+++ routes/rooms.js
@@ -38,12 +38,12 @@ var rooms = {
     "niceName": "the deveopers easter egg room",
     "description":"You see a set of doors that is blocked.",
     "search":{
-      "text": "You find the remains of a lan party",
+      "text": "You need a key in order to unlock this room.",
       "hasKeyText": "Look at the nerds that made this game.",
       "found": false
     },
     "keyNeeded":true,
-    "back":"durlandAuditorium",
+    "back":"beocat",
     "left":false,
     "right":false,
     "exit":false,
