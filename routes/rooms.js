016e8468edbd56c118aa5e09e58677a13b2dafcd
Zach Martin <zmironman@gmail.com>
2017-11-04 20:57:55 -0500

    fixed hasKeyText
    yayayayayayayayay

--- routes/rooms.js
+++ routes/rooms.js
@@ -7,6 +7,7 @@ var rooms = {
     "description":"You wake up in a dark, empty room",
     "search":{
       "text": "It seems that people were here, but left in a hurry",
+      "hasKeyText": "It seems that people were here, but left in a hurry.",
       "found": false
     },
     "keyNeeded":false,
@@ -22,6 +23,7 @@ var rooms = {
     "description":"You see a hallway full of doors.",
     "search":{
       "text": "You see a supercomputer",
+      "hasKeyText": "The door seems to open",
       "found": false
     },
     "keyNeeded":false,
@@ -37,6 +39,7 @@ var rooms = {
     "description":"You see a set of doors that is blocked.",
     "search":{
       "text": "You find the remains of a lan party",
+      "hasKeyText": "Look at the nerds that made this game.",
       "found": false
     },
     "keyNeeded":true,
@@ -52,6 +55,7 @@ var rooms = {
     "description":"You see a room full of powerful computers",
     "search":{
       "text": "You find the remains of a lan party",
+      "hasKeyText": "You find the remains of a lan party",
       "found": false
     },
     "keyNeeded":false,
@@ -67,6 +71,7 @@ var rooms = {
     "description":"You are in a hall surrounded by computer labs.",
     "search":{
       "text": "You see what looks like an exit staircase.",
+      "hasKeyText": "Congratulations, you WIN!",
       "found": false
     },
     "keyNeeded":false,
@@ -82,6 +87,7 @@ var rooms = {
     "description":"You see a room of tables, chairs, and a printer",
     "search":{
       "text": "You hear the sound of typing from somewhere in the room.",
+      "hasKeyText": "The typing seems to have stopped",
       "found": false
     },
     "keyNeeded":false,
@@ -97,6 +103,7 @@ var rooms = {
     "description":"You see a bunch of computers that could only run remote desktop.",
     "search":{
       "text": "You find a missing HDMI cable.",
+      "hasKeyText": "There seems to be displays missing HDMI cables",
       "found": false
     },
     "keyNeeded":false,
@@ -112,6 +119,7 @@ var rooms = {
     "description":"You see a staircase that leads to an exit",
     "search":{
       "text": "It looks like you need a key",
+      "hasKeyText": "Congratulations, you WIN!",
       "found": false
     },
     "keyNeeded":true,
@@ -127,6 +135,7 @@ var rooms = {
     "description":"You see a great hall ahead.",
     "search":{
       "text": "You find a fire extenguisher.",
+      "hasKeyText": "You find a fire extenguisher",
       "found": false
     },
     "keyNeeded":false,
@@ -142,6 +151,7 @@ var rooms = {
     "description":"You see a lab full of computers",
     "search":{
       "text": "You find a crying engineer.  You aren't sure if it is because everyone is missing, or they are an engineer.",
+      "hasKeyText": "The engineer seems to have left",
       "found": false
     },
     "keyNeeded":false,
@@ -157,6 +167,7 @@ var rooms = {
     "description":"You seem to be in a great room with a conference room and coffeshop.",
     "search":{
       "text": "You find tables and chairs, nothing useful.",
+      "hasKeyText": "You find tables and chairs, nothing useful.",
       "found": false
     },
     "keyNeeded":false,
@@ -172,6 +183,7 @@ var rooms = {
     "description":"You see a staircase that goes downstairs",
     "search":{
       "text": "I wonder what is downstairs",
+      "hasKeyText": "I wonder what is downstairs, could it be future DLC?",
       "found": false
     },
     "keyNeeded":false,
@@ -187,6 +199,7 @@ var rooms = {
     "description":"You see a library that doesn't have any books.",
     "search":{
       "text": "You smell the remains of sleepless nights and broken dreams.",
+      "hasKeyText": "You smell the remains of sleepless nights and broken dreams.",
       "found": false
     },
     "keyNeeded":false,
@@ -202,6 +215,7 @@ var rooms = {
     "description":"You see a computer lab and a door at the end of the hall.",
     "search":{
       "text": "You see a door at the end of the hall",
+      "hasKeyText": "ou see a door at the end of the hall, I wonder if you can open it with this key?",
       "found": false
     },
     "keyNeeded":false,
@@ -216,7 +230,8 @@ var rooms = {
     "niceName": "the library exit",
     "description":"You see a library that doesn't have any books.",
     "search":{
-      "text": "Congratulations, you WIN!",
+      "text": "The door seems to be locked.",
+      "hasKeyText": "Congratulations, you WIN!",
       "found": false
     },
     "keyNeeded":true,
@@ -232,6 +247,7 @@ var rooms = {
     "description":"The smell of the coffee is strong",
     "search":{
       "text": "You search the bar, and find a key!",
+      "hasKeyText": "You remember that the key was on the bar",
       "found":{
         "key": true,
         "map": false,
@@ -250,7 +266,8 @@ var rooms = {
     "niceName": "at the exit by rahdeenas coffee shop",
     "description":"You see a locked exit",
     "search":{
-      "text": "You find an exit that seems to be welded shut.",
+      "text": "You find an exit that seems to be closed and locked",
+      "hasKeyText": "Congratulations, you WIN!",
       "found": false
     },
     "keyNeeded":true,
@@ -266,6 +283,7 @@ var rooms = {
     "description":"You see a digital board.",
     "search":{
       "text": "You find a map on the board!",
+      "hasKeyText": "You find a map on the board!",
       "found":{
        "key": false,
        "map": true,
@@ -281,10 +299,11 @@ var rooms = {
   },
 
   "durlandAuditorium":{
-    "niceName": "pasely auditorium",
+    "niceName": " the entrance to pasely auditorium",
     "description":"You see a big auditorium and glass doors",
     "search":{
       "text": "You see a picture of a man that seems important.",
+      "hasKeyText": "You see a picture of a man that seems important.",
       "found": false
     },
     "keyNeeded":false,
@@ -300,6 +319,7 @@ var rooms = {
     "description":"You see a set of doors that is blocked.",
     "search":{
       "text": "You find the remains of a lan party",
+      "hasKeyText": "You find the remains of a lan party",
       "found": false
     },
     "keyNeeded":true,
@@ -315,6 +335,7 @@ var rooms = {
     "description":"You see a set of doors that is blocked.",
     "search":{
       "text": "you see a window to the left and computer lab to the right",
+      "hasKeyText": "you see a window to the left and computer lab to the right",
       "found": false
     },
     "keyNeeded":false,
@@ -330,6 +351,7 @@ var rooms = {
     "description":"You see a set of doors that is blocked.",
     "search":{
       "text": "You see how nice the outside world is.",
+      "hasKeyText": "You see how nice the outside world is.",
       "found": false
     },
     "keyNeeded":false,
