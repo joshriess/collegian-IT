41bfeb15c33cc1bd741c48d35f09432d612928a5
pkmnct <george@georgewwalker.com>
2017-11-06 13:45:09 -0600

    Remove unneeded files

--- hack-kstate-escape/routes/imageServer.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-express.static('images');
-router.use(express.static('images'));
-router.use('/images', express.static('images'));
-
-module.exports = router;
